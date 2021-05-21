736994159498fc748e6b2b18c6b354a0bf9a4846
rabbanibcs <rabbanibcs@gmail.com>
2021-05-22 05:53:13 +0600

    snippet url connected

--- tutorial/tutorial/tutorial/urls.py
+++ tutorial/tutorial/tutorial/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from quickstart.views import UserViewSets,GroupViewSet
+from snippets import urls
 
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSets)
@@ -13,5 +14,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls',namespace='rest_framework')),
-
+    path('serial/', include(urls)),
 ]
